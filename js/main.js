401be863205b1b4a27074196720d4ac5029afadb
Dussart Guillaume <dussartguillaume@orange.fr>
2017-07-28 11:37:47 +0200

    main.js->add sort

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 function loadDoc() {
+    $('.table').append("<thead><tr><th>First Name</th><th>Last Name</th><th>Age</th><th>Country</th></tr></thead>");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        var myObj = JSON.parse(this.responseText)
         if (this.readyState == 4 && this.status == 200) {
-            for (var i in myObj) {
-                document.getElementById("demo").innerHTML = myObj.clients[1].name;
+            var myObj = JSON.parse(this.responseText);
+            myObj.clients.sort(function(a,b){
+                if(a.lastname<b.lastname){return-1}
+                if(a.lastname>b.lastname){return 1}
+            })
+            for (var i in myObj.clients) {
+                $('.table').append("<tbody><tr><th>"+myObj.clients[i].firstname+
+                    "</th><td>"+myObj.clients[i].lastname+"</td><td>"+myObj.clients[i].age+
+                    "</td><td>"+myObj.clients[i].city+"</td></tr>"
+                );
             }
         }
     };
     xhttp.open("GET", "clients.json", true);
     xhttp.send();
 }
+$('select').change(function(){
+    console.log($('select').val())
+})
