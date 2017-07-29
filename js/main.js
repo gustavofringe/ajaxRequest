61f5fa3846bec893eddd6c026a2cc88e0123ae6d
Guillaume <dussartguillaume@orange.fr>
2017-07-29 19:14:55 +0200

    main.js->end to replace table

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.table').append("<thead><tr><th>First Name</th><th>Last Name</th><th>Age</th><th>Country</th></tr></thead>");
+//$('.table').append("<thead><tr><th>First Name</th><th>Last Name</th><th>Age</th><th>Country</th></tr></thead>");
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -6,11 +6,28 @@ xhttp.onreadystatechange = function () {
         $('#select').change(function () {
             people = sortByKey(myObj.clients, $('#select').val());
             console.table(people)
+            $('#display').empty()
+            for (var i in myObj.clients) {
+                $('#display').append("<tr><th class='first'>" + myObj.clients[i].Firstname +
+                    "</th><td class='last'>" + myObj.clients[i].Lastname + "</td><td class='age'>" + myObj.clients[i].Age +
+                    "</td><td class='country'>" + myObj.clients[i].Country + "</td></tr>"
+                );
+            }
         });
+        $('#add').click(function(){
+            myObj.clients.push({
+                Firstname: $('.fname').val(),
+                Lastname: $('.lname').val(),
+                Age: $('.age').val(),
+                Country: $('.country').val()
+            });
+            $('.table').append(myObj.clients)
+        })
+        console.log(myObj.clients)
         for (var i in myObj.clients) {
-            $('.table').append("<tbody><tr><th>" + myObj.clients[i].Firstname +
-                "</th><td>" + myObj.clients[i].Lastname + "</td><td>" + myObj.clients[i].Age +
-                "</td><td>" + myObj.clients[i].Country + "</td></tr>"
+            $('#display').append("<tr><th class='first'>" + myObj.clients[i].Firstname +
+                "</th><td class='last'>" + myObj.clients[i].Lastname + "</td><td class='age'>" + myObj.clients[i].Age +
+                "</td><td class='country'>" + myObj.clients[i].Country + "</td></tr>"
             );
         }
     }
